7ced220caf33dff8e95ca8d8fe6dd70a0b98df19
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-20 22:41:43 +0530

    operators in javascript

---
+++ operators.js
@@ -0,0 +1,64 @@
+console.log("operators in javascript")
+/*
+ arithmetic operator
++ , - , * , ** , / , % , ++ , -- 
+
+assignment operator
+= , += , -= , /= , %= , **= 
+
+comparsion operator 
+== , != , ===(equal value and type) , !== , < , > , <= ,>= , ? 
+
+logical operator 
+&& , ! , || 
+*/
+
+
+console.log("Operators in Js")
+// Arithmetic Operators 
+let a = 10;
+let b = 4;
+console.log("a + b = ", a + b)
+console.log("a - b = ", a - b)
+console.log("a / b = ", a / b)
+console.log("a ** b = ", a ** b)
+console.log("a % b = ", a % b)
+console.log("++a = ", ++a)
+console.log("a++ = ", a++)
+console.log("--a = ", --a)
+console.log("a-- = ", a--)
+console.log("a = ", a)
+console.log("a-- = ", a--)
+
+// Assignment Operators 
+let assignment = 1;
+assignment += 5 // same as assignment = assignment + 5
+console.log("a is now = ", a)
+assignment -= 5 // same as assignment = assignment - 5
+console.log("a is now = ", a)
+assignment *= 5 // same as assignment = assignment * 5
+console.log("a is now = ", a)
+assignment /= 5 // same as assignment = assignment / 5
+console.log("a is now = ", a)
+
+// Comparison Operators 
+let comp1 = 6;
+let comp2 = 7;
+console.log("comp1 == comp2 is ", comp1 == comp2)
+console.log("comp1 != comp2 is ", comp1 != comp2)
+console.log("comp1 === comp2 is ", comp1 === comp2)
+console.log("comp1 !== comp2 is ", comp1 !== comp2)
+console.log("comp1 > comp2 is ", comp1 > comp2)
+
+// Logical Operators
+let x = 5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+console.log(!true)
+
+// Comments
+// This is a single line comment
+
+
