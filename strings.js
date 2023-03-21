16737a15e05e4a94a84595bfd7eb35f5ce4ffa3e
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-22 01:00:56 +0530

    string and escape sequence and templates

---
+++ strings.js
@@ -0,0 +1,21 @@
+// string decl
+let name = "akash"
+console.log(name.length)
+
+let friend = 'vardaan'
+console.log(friend[1])
+
+
+// template literals
+
+let boy1 = "akashh"
+let boy2 = "vardaan"
+
+let s = `${boy2} is a friend of ${boy1}`
+console.log(s)
+
+//escape sequence characters 
+let friut = 'bana\'na'
+console.log(friut)
+
+
