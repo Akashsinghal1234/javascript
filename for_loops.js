c260c2bd4820dfa5df8af1d3a5bf5bf218c0943f
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-22 00:13:47 +0530

    for loops in javascript

---
+++ for_loops.js
@@ -0,0 +1,39 @@
+// sum of n natural numbers 
+/*
+alert("akash this side bro ;) ")
+
+let sum = 0 
+let n = prompt("enter the values to n ") 
+n = Number.parseInt(n)
+for(let i = 0; i< n; i++){
+    sum += (i+1)
+}
+console.log(n + sum)
+
+
+*/
+
+/* for in loop 
+let obj = {
+    akash : 90 , 
+    kaku : 80 , 
+    vardaan : 90 , 
+    giiglu : 80
+}
+
+for (let a in obj){
+    console.log(a + obj[a])
+}
+*/
+
+// for of loop 
+/*
+for (let b in akash){
+    console.log(a + obj[b])
+}
+
+*/
+
+
+
+
