487ca5a07939298eae2385408ea9d93060504908
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-22 00:15:23 +0530

    while loops in javascript

---
+++ while.js
@@ -0,0 +1,22 @@
+//while loop
+ /*
+let n = prompt("enter value ")
+n = Number.parseInt(n)
+
+let i = 0;
+while(i<n){
+    console.log(i)
+    i++;
+}
+*/
+
+//do while loop 
+
+let n = prompt("enter value ")
+n = Number.parseInt(n)
+
+let i = 0;
+do{
+    console.log(i)
+    i++;
+}while(i<n)
