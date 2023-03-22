e6926860ba219e046d070d107f56eb00cca7964c
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-23 00:07:57 +0530

    arrays in javascript

---
+++ arrays.js
@@ -0,0 +1,13 @@
+alert("so we start a important topic ARRAYS ;;;;;;))))))")
+
+let a = [99, 98, 67, 87, 56, "akash", null]
+
+console.log(a)
+console.log(a[3])
+
+console.log(a.length)
+
+a[0] = 100
+
+//array is an object 
+
