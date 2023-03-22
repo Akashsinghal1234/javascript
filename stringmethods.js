462aa0591e50173baee4dd152f642c34efb24bec
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-23 00:10:29 +0530

    for loops in array , practice sets and string methods

---
+++ stringmethods.js
@@ -0,0 +1,25 @@
+alert("buddy we will study about the string methods :)")
+alert("strings are immutable ;)")
+let a = "akash"
+console.log(a.length)
+
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+
+console.log(a.slice(2,3))
+
+console.log(a.replace("aka","kak"))
+
+
+let b = kaku
+console.log(a.concat(b))
+
+let c = "  akki  "
+console.log(c.trim())
+
+let fr = "aakkaassh"
+console.log(fr[0])
+console.log(fr[1])
+
+
+
