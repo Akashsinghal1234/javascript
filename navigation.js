e3690d4c49aa6366a919224a4de82976ccc5d51e
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-25 01:09:09 +0530

    navigation bar bootstrap

---
+++ navigation.js
@@ -0,0 +1,10 @@
+const changebodyred = () => {
+    document.body.firstElementChild.style.background = "red"
+}
+
+
+
+
+let b = document.body
+console.log("first child" , b.firstChild)
+console.log("first element" , b.firstElementChild)
