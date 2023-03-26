537d8cfe4c23600e1fc79752b874dbb730276b4f
Akashsinghal1234 <singhlakash1234@gmail.com>
2023-03-27 00:47:33 +0530

    practice set

--- conditional.js
+++ conditional.js
@@ -1,37 +1,37 @@
-/* 
-if
-if .. else 
-if else .. if else 
-*/
+// /* 
+// if
+// if .. else 
+// if else .. if else 
+// */
 
-//parseInt method to convert string to number 
+// //parseInt method to convert string to number 
 
-let a = prompt("hey this side akash")
-a = Number.parseInt(a)
-if (a>0){
-    alert("this is good age")
-}
+// let a = prompt("hey this side akash")
+// a = Number.parseInt(a)
+// if (a>0){
+//     alert("this is good age")
+// }
 
-else {
-    alert("this in not good buddy")
-}
+// else {
+//     alert("this in not good buddy")
+// }
 
-let a = prompt("Hey whats you age?");
-a = Number.parseInt(a); // Converting the string to a number
-if(a<0){
-  alert("This is an invalid age");
-}
-else if(a<9){
-  alert("You are a kid and you cannot even think of driving");
-}
-else if(a<18 && a>=9){
-  alert("You are a kid and you can think of driving after 18");
-}
-else{
-  alert("You can now drive as you are above 18");
-}
-console.log("Done")*/
-// HomeWork - Explore switch statement and write a basic program in the comments
+// let a = prompt("Hey whats you age?");
+// a = Number.parseInt(a); // Converting the string to a number
+// if(a<0){
+//   alert("This is an invalid age");
+// }
+// else if(a<9){
+//   alert("You are a kid and you cannot even think of driving");
+// }
+// else if(a<18 && a>=9){
+//   alert("You are a kid and you can think of driving after 18");
+// }
+// else{
+//   alert("You can now drive as you are above 18");
+// }
+// console.log("Done")*/
+// // HomeWork - Explore switch statement and write a basic program in the comments
 
-//ternary operator
-console.log("You can", (a<18? "not drive" :"drive"))
+// //ternary operator
+// console.log("You can", (a<18? "not drive" :"drive"))
---
+++ setIntervalSetTimeout.js
@@ -0,0 +1,33 @@
+document.write("hello")
+
+const sum = (a,b) => a+b 
+
+
+
+setTimeout(function() {
+    alert("hello set time out example")
+},5000 ) 
+
+
+
+document.write("Hello")
+
+//const sum = (a, b, c) => {
+  console.log("Yes I am running " + (a + b + c))
+  a + b
+///}
+
+setTimeout(sum, 1000, 1, 2, 7)
+// setInterval(function() {
+//   alert("setinterval")
+// }, 3000)
+
+// let a = setTimeout(function() {
+//   alert("I am inside of settimeout")
+// }, 5000)
+
+// let b = prompt("Do you want to run the settimout?")
+// if ("n" == b) {
+//   clearTimeout(a)
+// }
+// console.log(a)
